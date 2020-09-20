2cf7ccd5bff48b9225a6d18e03989f5f8dce8e2c
imdj2001 <65225898+imdj2001@users.noreply.github.com>
2020-09-20 13:05:42 +0530

    Add files via upload

---
+++ build/precache-manifest.34b69002507b91fcd1370e355cd6f306.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea280cb96ac3009b205940c6c8e1fa33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4bff3cf9951cbf63163",
+    "url": "/static/css/main.cded74a8.chunk.css"
+  },
+  {
+    "revision": "0e5235b871a34b2023e7",
+    "url": "/static/js/2.e6c06828.chunk.js"
+  },
+  {
+    "revision": "cadb4f4acce959edd474e83a8c4e046a",
+    "url": "/static/js/2.e6c06828.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4bff3cf9951cbf63163",
+    "url": "/static/js/main.c9dc7f16.chunk.js"
+  },
+  {
+    "revision": "07b33697149324db54e5",
+    "url": "/static/js/runtime-main.b8a87127.js"
+  },
+  {
+    "revision": "84a98a0babf19d74ff191ada8964ab64",
+    "url": "/static/media/logosilver.84a98a0b.jpeg"
+  }
+]);
---
+++ src/Components/Banner.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import requests from "../requests";
+import "./Banner.css";
+
+function Banner({ history }) {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      return request;
+    }
+    fetchData();
+  }, []);
+
+  //for 3 ... when the words are more
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
+
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url(
+             "https://image.tmdb.org/t/p/original/${movie?.backdrop_path}"
+         )`,
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="banner__contents" key={movie.id} history={history}>
+        {/* tittle*/}
+        <h1 ClassName="banner__title">
+          {movie?.tittle || movie?.name || movie?.original_name}
+        </h1>
+        {/* 2 button div*/}
+        {/* <div className="banner__buttons">
+          <button className="banner__button"
+          onClick={() => {
+            history.push(`/moviedetail/${movie?.id}`);
+          }}>Info</button>
+       
+        </div> */}
+        {/*description */}
+        <h1 className="banner__description">
+          {truncate(movie?.overview, 250)}
+        </h1>
+      </div>
+
+      <div className="banner--fadeBottom" />
+    </header>
+  );
+}
+
+export default Banner;
